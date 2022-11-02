95ec7cf588646ab5fa88ec44c3fbb3224dfb36f0
jsju4020-admin <jsju6076@gmail.com>
2022-11-02 10:41:07 +0900

    initital coommit

---
+++ src/main/java/com/kanozo/MyKanozoApplication.java
@@ -0,0 +1,13 @@
+package com.kanozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKanozoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyKanozoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanozo/MyKanozoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanozo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKanozoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
